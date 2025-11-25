367b8164db1832f0c053654d9902323fa9e98e3b
SergWebAcceptance <serg.web@acceptance.ltd>
2025-11-25 14:33:54 +0200

    Enhance header navigation with active link styles
    - Added active link styles in SCSS to highlight the current page in the header navigation.
    - Updated Header component to apply active styles based on the current pathname for improved user experience and visual feedback.

--- src/shared/ui/components/header/Header.tsx
+++ src/shared/ui/components/header/Header.tsx
@@ -19,6 +19,7 @@ export const Header = () => {
 
   useEffect(() => {
     setIsMobileMenuOpen(false);
+    console.log(pathname);
   }, [pathname]);
 
   return (
@@ -37,17 +38,33 @@ export const Header = () => {
           </button>
 
           <nav className={styles.header__menu}>
-            <Link href="/team">{t('team', { fallback: 'Team' })}</Link>
-            <Link href="/the-integrator-model">
+            <Link href="/team" className={pathname === '/team' ? styles.active : ''}>
+              {t('team', { fallback: 'Team' })}
+            </Link>
+            <Link
+              href="/the-integrator-model"
+              className={pathname === '/the-integrator-model' ? styles.active : ''}
+            >
               {t('the-integrator-model', { fallback: 'The Integrator Model' })}
             </Link>
-            <Link href="/expertise-and-services">
+            <Link
+              href="/expertise-and-services"
+              className={pathname === '/expertise-and-services' ? styles.active : ''}
+            >
               {t('expertise-and-services', { fallback: 'Expertise and Services' })}
             </Link>
-            <Link href="/industries">{t('industries', { fallback: 'Industries' })}</Link>
-            <Link href="/about-us">{t('about-us', { fallback: 'About Us' })}</Link>
-            <Link href="/insights">{t('insights-and-news', { fallback: 'Insights & News' })}</Link>
-            <Link href="/contact">{t('contact', { fallback: 'Contact' })}</Link>
+            <Link href="/industries" className={pathname === '/industries' ? styles.active : ''}>
+              {t('industries', { fallback: 'Industries' })}
+            </Link>
+            <Link href="/about-us" className={pathname === '/about-us' ? styles.active : ''}>
+              {t('about-us', { fallback: 'About Us' })}
+            </Link>
+            <Link href="/insights" className={pathname === '/insights' ? styles.active : ''}>
+              {t('insights-and-news', { fallback: 'Insights & News' })}
+            </Link>
+            <Link href="/contact" className={pathname === '/contact' ? styles.active : ''}>
+              {t('contact', { fallback: 'Contact' })}
+            </Link>
           </nav>
           <div className={styles.header__contact}>
             <Button variant="secondary" url="/consultation-request-form" type="link">
@@ -58,17 +75,33 @@ export const Header = () => {
       </div>
       <div className={`${styles.header__mobile_menu} ${isMobileMenuOpen ? styles.open : ''}`}>
         <nav>
-          <Link href="/team">{t('team', { fallback: 'Team' })}</Link>
-          <Link href="/the-integrator-model">
+          <Link href="/team" className={pathname === '/team' ? styles.active : ''}>
+            {t('team', { fallback: 'Team' })}
+          </Link>
+          <Link
+            href="/the-integrator-model"
+            className={pathname === '/the-integrator-model' ? styles.active : ''}
+          >
             {t('the-integrator-model', { fallback: 'The Integrator Model' })}
           </Link>
-          <Link href="/expertise-and-services">
+          <Link
+            href="/expertise-and-services"
+            className={pathname === '/expertise-and-services' ? styles.active : ''}
+          >
             {t('expertise-and-services', { fallback: 'Expertise and Services' })}
           </Link>
-          <Link href="/industries">{t('industries', { fallback: 'Industries' })}</Link>
-          <Link href="/about-us">{t('about-us', { fallback: 'About Us' })}</Link>
-          <Link href="/insights">{t('insights-and-news', { fallback: 'Insights & News' })}</Link>
-          <Link href="/contact">{t('contact', { fallback: 'Contact' })}</Link>
+          <Link href="/industries" className={pathname === '/industries' ? styles.active : ''}>
+            {t('industries', { fallback: 'Industries' })}
+          </Link>
+          <Link href="/about-us" className={pathname === '/about-us' ? styles.active : ''}>
+            {t('about-us', { fallback: 'About Us' })}
+          </Link>
+          <Link href="/insights" className={pathname === '/insights' ? styles.active : ''}>
+            {t('insights-and-news', { fallback: 'Insights & News' })}
+          </Link>
+          <Link href="/contact" className={pathname === '/contact' ? styles.active : ''}>
+            {t('contact', { fallback: 'Contact' })}
+          </Link>
         </nav>
         <div className={styles.header__mobile_menu_contact}>
           <Button variant="secondary" url="/consultation-request-form" type="link">
