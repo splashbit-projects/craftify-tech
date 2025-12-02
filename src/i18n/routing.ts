5dde4e4d55e0358b45cf93c09042f1236d709c2f
SergWebAcceptance <serg.web@acceptance.ltd>
2025-12-02 16:03:36 +0200

    Update Romanian localization and adjust middleware routing settings
    - Changed the Romanian translation for "Decline" in `ro.json` to "Declin" for improved accuracy.
    - Updated middleware configuration in `middleware.ts` to disable locale detection for better routing control.
    - Modified routing settings in `routing.ts` to always use locale prefixes and changed the default locale from Romanian to English for enhanced consistency.

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -3,7 +3,7 @@ import { defineRouting } from 'next-intl/routing';
 export const routing = defineRouting({
   // A list of all locales that are supported
   locales: ['ro', 'en', 'de', 'it'],
-  localePrefix: 'as-needed',
+  localePrefix: 'always',
   // Used when no locale matches
-  defaultLocale: 'ro',
+  defaultLocale: 'en',
 });
--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,10 @@ import createMiddleware from 'next-intl/middleware';
 
 import { routing } from './i18n/routing';
 
-export default createMiddleware(routing);
+export default createMiddleware({
+  ...routing,
+  localeDetection: false, 
+});
 
 export const config = {
   // Match all pathnames except for
