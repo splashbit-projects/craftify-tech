19292ce6820f982ab8fbe56f533daef07aa3c0cc
SergWebAcceptance <serg.web@acceptance.ltd>
2025-12-03 10:11:58 +0200

    Update default locale in routing settings from English to Romanian for improved localization support

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -5,5 +5,5 @@ export const routing = defineRouting({
   locales: ['ro', 'en', 'de', 'it'],
   localePrefix: 'as-needed',
   // Used when no locale matches
-  defaultLocale: 'en',
+  defaultLocale: 'ro',
 });
